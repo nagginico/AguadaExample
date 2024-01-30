d7ab7f1af6268c99fe57484047bf1c14e874356d
nagginico <nagginico@gmail.com>
2024-01-30 15:59:34 -0300

    commit

--- Aguada/settings.py
+++ Aguada/settings.py
@@ -123,10 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
