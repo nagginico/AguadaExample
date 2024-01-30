783ef6a9f6f5a3a7ee3928256dc6447e45217317
nagginico <nagginico@gmail.com>
2024-01-30 16:38:18 -0300

    commit 'Actualizacion'

--- Aguada/settings.py
+++ Aguada/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (os.path.join (BASE_DIR,'static'),)
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
